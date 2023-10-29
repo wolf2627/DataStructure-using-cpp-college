cfc1580dbe1d2d4c374c9ba9260b9b824607a022
aswin <aswins22cs@psnacet.edu.in>
2023-10-29 12:21:06 +0530

    implementation of stack using array, first commit

---
+++ stackarray.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+const int maxsize = 100;
+
+class Stack {
+    private: 
+        int a[maxsize];
+        int top;
+    public:
+        int size, x;
+        Stack() {
+            top = -1;
+            cout << "Enter the size of the stack: ";
+            cin >> size;
+        }
+
+        bool isFull() {
+            return (top == size - 1);
+        }
+
+        bool isEmpty() {
+            return top == -1;
+        }
+
+        void push() {
+            if (isFull()) {
+                cout << "Stack is full.So, you can't push element" << endl;
+            } else {
+                cout<<"Enter element to push: ";
+                cin>>x;
+                top++;
+                a[top] = x;
+                cout<<"Element "<<x<<" pushed successfully"<<endl;
+            }
+        }
+
+        int pop() {
+            if (isEmpty()) {
+                cout << "Stack is empty. So you can't pop element" << endl;
+                return -1;
+            } else {
+                x = a[top];
+                top--;
+                return x;
+            }
+        }
+
+        void display() {
+            if (isEmpty()) {
+                cout << "There are no elements to display" << endl;
+            } else {
+                cout <<"\nStack" << endl;
+                for (int i = top; i >= 0; i--)
+                    cout<< a[i] <<endl;
+                cout << endl;
+            }
+        }
+
+        void peak() {
+            if (isEmpty()) {
+                cout << "Stack is empty, hence no peek value" << endl;
+            } else {
+                cout << "The peek value is : " << a[top] << endl;
+            }
+        }
+};
+
+int main() {
+    Stack st;
+    int ch, ele;
+    char c;
+    do {
+        cout << "What do you want to do?" << endl;
+        cout << "(1).PUSH\n(2).POP\n(3).DISPLAY\n(4).FIND PEAK\n(5).EXIT" << endl;
+        cout<<"Enter your choice: ";
+        cin >> ch;
+        switch(ch) {
+            case 1:
+                st.push();
+                break;
+            case 2:
+                ele= st.pop();
+                if(ele!=-1)
+                    cout << "The popped element from the stack is: " <<ele<< endl;
+                break;
+            case 3:
+                st.display();
+                break;
+            case 4:
+                st.peak();
+                break;
+            case 5:
+                exit(0);
+            default:
+                cout << "Enter a valid choice" << endl;
+        }
+        cout << "Press any character to continue...";
+        cin >> c;
+    } while (true);
+}
